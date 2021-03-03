c32e6348bdf5d4309a1c1f2a640228f05d917619
Daniel <bergstroem.daniel@gmail.com>
2021-03-03 14:21:20 +0100

    test

---
+++ forelasning7/src/forelasning7/FizzBuzzer.java
@@ -0,0 +1,28 @@
+package forelasning7;
+
+public class FizzBuzzer {
+
+	public String getValue(int input) {
+		
+		String s = "";
+		
+		if(input % 3 == 0 && input % 5 == 00) {
+			return "FizzBuzz";
+		}
+		
+		if(input % 3 == 0) {
+			return "Fizz";
+		}
+		
+		if(input % 5 == 0) {
+			return "Buzz";
+		}
+		
+		
+		
+		
+		//return Integer.toString(input);
+		
+	}
+
+}
